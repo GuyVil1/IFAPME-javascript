2c440549d5ce01b1fd99665f0fe0093fef9a5629
GuyVil1 <guy.vilain1@gmail.com>
2021-03-17 22:24:45 +0100

    pratique sur les tableaux ok

---
+++ pratique/exemplePendu.js
@@ -0,0 +1,21 @@
+let mot = "bonjour";
+let entréeUser = "b";
+let lettreTrouvée = [];
+
+for( i = 0; i<= (mot.length -1); i++){
+    lettreTrouvée.push('-');
+}
+
+console.log(lettreTrouvée);
+
+//demander à mon user de rentrer une lettre.
+
+for( i = 0; i<= (mot.length -1); i++){
+    //vérifie si entréeUser == mot[i];
+    if(entréeUser == mot[i]){
+        lettreTrouvée[i] = entréeUser;
+    }
+}
+
+console.log(lettreTrouvée);
+
